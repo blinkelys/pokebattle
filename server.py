5e3b393035555554ab7a8fc64753a63565f16fb2
blinkelys <martinustomrenslemmen@gmail.com>
2026-09-17 15:28:30 +0200

    feat: ui

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+pokemons = [
+    {"id": 1, "name": "Bulbasaur"},
+    {"id": 2, "name": "Ivysaur"},
+    {"id": 3, "name": "Venusaur"},
+]
+
+@app.route("/api/all-pokemons")
+def get_pokemons():
+    return {"pokemons": pokemons}
 
 if __name__ == "__main__":
     app.run(debug=True)
